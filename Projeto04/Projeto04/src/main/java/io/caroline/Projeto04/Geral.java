a54c2099b9d8c031e58ea0fc882bea352caca51d
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-03-05 10:57:01 -0300

    feat: Módulo 5 propriedades internas e externas

---
+++ Projeto04/Projeto04/src/main/java/io/caroline/Projeto04/Geral.java
@@ -0,0 +1,25 @@
+package io.caroline.Projeto04;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Geral implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Value("${geral.nome}")
+    private String nome;
+
+    @Value("${my.server}")
+    private String server;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        System.out.println(nome);
+        System.out.println(server);
+
+    }
+
+
+}
